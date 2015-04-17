7d1004a0f33dd4ed68915afabddec075563d79a1
bern422 <bern422@gmail.com>
2015-04-17 00:21:51 -0400

    unbroke the build sorry mom

--- game/src/model/entity/Avatar.java
+++ game/src/model/entity/Avatar.java
@@ -1,5 +1,8 @@
 package model.entity;
 
+import model.entity.occupation.CharacterOccupation;
+import model.map.Location;
+
 public class Avatar extends Entity {
 	
     public Avatar(Location location, CharacterOccupation occupation) {
--- game/src/model/entity/Entity.java
+++ game/src/model/entity/Entity.java
@@ -1,36 +1,23 @@
 package model.entity;
 
-<<<<<<< HEAD
-import model.item.Inventory;
-import model.map.Location;
-import model.occupation.*;
-=======
-import model.entity.occupation.Occupation;
+import model.entity.occupation.*;
 import model.item.Inventory;
 import model.item.TakeableItem;
 import model.map.Location;
 import model.map.MapPlaceable;
->>>>>>> 9a39a32fb0883f0f61a723f6d2a9722dd040e09e
 
 public class Entity implements MapPlaceable {
 	Location location;
-<<<<<<< HEAD
 	Occupation occupation;
 	Inventory inventory;
 	
 	public Entity () {
-		location = new Location();
+		location = new Location(0,0);
 		occupation = new Smasher();
 	}
 	
-	public Entity(Occupation occupation) {
-=======
-	model.entity.occupation.Occupation occupation;
-	Inventory inventory;
-
 	public Entity(Location location, Occupation occupation) {
 		this.location = location;
->>>>>>> 9a39a32fb0883f0f61a723f6d2a9722dd040e09e
 		this.occupation = occupation;
 	}
 	
@@ -51,7 +38,7 @@ public class Entity implements MapPlaceable {
 		this.occupation = occupation;
 	}
 
-	public void addIteam(TakeableItem item) {
+	public void addItem(TakeableItem item) {
 		inventory.addItem(item);
 	}
 }
